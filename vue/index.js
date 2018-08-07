f8b972d1cec7d799567683cd62a76a40e4daa19f
hfq-lizhiliang <lizhiliang@huizhaofang.com>
2018-08-07 23:26:46 +0800

    v-for 循环

--- vue/index.js
+++ vue/index.js
@@ -8,7 +8,7 @@ var app = new Vue({
 });
 
 app.message = 'this is first vue';
-// app.seen = false;
+
 
 var app2 = new Vue({
     el: '#app-2',
@@ -16,3 +16,26 @@ var app2 = new Vue({
         message: '页面加载于 ' + new Date().toLocaleString()
     }
 });
+// 控制台输入 aapp2.message="新消息"，鼠标悬浮在上面一会，会出现新消息三个字
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+});
+app3.seen = false;
+
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            {text: '学习 JavaScript'},
+            {text: '学习 Vue'},
+            {text: '整个牛项目'}
+        ]
+    }
+});
+//在控制台输入 app4.todos.push({ text: '新项目' })，会在列表后面添加一个新元素
+
